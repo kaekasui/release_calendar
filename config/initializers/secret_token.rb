1723c7b0324579cc4c0e1719b782beabf81f2360
mayu <white_hot_chocolate@i.softbank.jp>
2012-11-30 12:48:29 +0900

    refs #100 : 新規プロジェクト作成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Release::Application.config.secret_token = 'bee751cd17cd5100d91b7d61cafe9f39815b7c0a14ea997a7e55c62d9851d721e406c4ecbae57d8c8944ef8dedb7ab352ffb8ecdc5e8263bcf6006133bde0c45'
